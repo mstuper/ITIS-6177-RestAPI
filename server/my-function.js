1a6aad72e632497e56d15547d182606f0ccb08f3
mstuper <mstuper@uncc.edu>
2023-02-24 16:42:14 +0000

    delete the py file

--- server/my-function.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  const name = 'Michael Stuper';
-  const keyword = event.queryStringParameters.keyword;
-  const response = `${name} says ${keyword}`;
-  return {
-    statusCode: 200,
-    body: response
-  };
-};
